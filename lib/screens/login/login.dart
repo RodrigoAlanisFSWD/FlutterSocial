216f8fa2e34cdd49d32245e5e98e9596a65d9af0
Rodrigo Alanis Lemus <rodrigoalanislemus@Rodrigos-MacBook-Air.local>
2022-08-06 13:36:30 -0500

    Login Form and Register Form Bloc Integration (Not Tested)

--- lib/screens/login/login.dart
+++ lib/screens/login/login.dart
@@ -1,7 +1,9 @@
+import 'package:client/bloc/login/login_bloc.dart';
 import 'package:client/widgets/Button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Login extends StatefulWidget {
   const Login({Key? key}) : super(key: key);
@@ -23,63 +25,84 @@ class _LoginState extends State<Login> {
 
   @override
   Widget build(BuildContext context) {
+    _onLoginSubmit() {
+      BlocProvider.of<LoginBloc>(context)
+          .add(LoginSubmit(email: _email.text, password: _password.text));
+    }
+
     return Scaffold(
-      appBar: AppBar(title: const Text("Sign In")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 350,
-              child: TextField(
-                controller: _email,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    label: Text("Email"), prefixIcon: Icon(Icons.email)),
+        appBar: AppBar(title: const Text("Sign In")),
+        body: BlocListener(
+          listener: ((context, state) {
+            if (state is LoginFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text("Sign In Failed"),
+                backgroundColor: Colors.red,
+              ));
+            }
+          }),
+          child: BlocBuilder(builder: ((context, state) {
+            return Center(
+              child: Column(
+                mainAxisAlignment: MainAxisAlignment.center,
+                children: [
+                  SizedBox(
+                    width: 350,
+                    child: TextField(
+                      controller: _email,
+                      keyboardType: TextInputType.emailAddress,
+                      decoration: const InputDecoration(
+                          label: Text("Email"), prefixIcon: Icon(Icons.email)),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(10)),
+                  SizedBox(
+                    width: 350,
+                    child: TextField(
+                      controller: _password,
+                      obscureText: true,
+                      decoration: const InputDecoration(
+                          label: Text("Password"),
+                          prefixIcon: Icon(Icons.lock)),
+                    ),
+                  ),
+                  const Padding(padding: EdgeInsets.all(20)),
+                  Button(
+                      size: const Size(350, 50),
+                      text: "Sign In",
+                      onPressed: () {
+                        _onLoginSubmit();
+                      }),
+                  const Padding(padding: EdgeInsets.all(5)),
+                  OutlinedButton.icon(
+                      onPressed: () {},
+                      style: OutlinedButton.styleFrom(
+                          minimumSize: const Size(350, 50)),
+                      icon: Image.asset(
+                        'assets/google.png',
+                        width: 25,
+                        height: 25,
+                      ),
+                      label: const Text(
+                        "Continue With Google",
+                        style: TextStyle(color: Colors.white),
+                      )),
+                  Row(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Text("You Dont Have An Account ?"),
+                      TextButton(
+                          onPressed: () {
+                            Navigator.pushNamed(context, "/sign-up");
+                          },
+                          child: const Text("click here"))
+                    ],
+                  )
+                ],
               ),
-            ),
-            const Padding(padding: EdgeInsets.all(10)),
-            SizedBox(
-              width: 350,
-              child: TextField(
-                controller: _password,
-                obscureText: true,
-                decoration: const InputDecoration(
-                    label: Text("Password"), prefixIcon: Icon(Icons.lock)),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(20)),
-            Button(
-                size: const Size(350, 50), text: "Sign In", onPressed: () {}),
-            const Padding(padding: EdgeInsets.all(5)),
-            OutlinedButton.icon(
-                onPressed: () {},
-                style:
-                    OutlinedButton.styleFrom(minimumSize: const Size(350, 50)),
-                icon: Image.asset(
-                  'assets/google.png',
-                  width: 25,
-                  height: 25,
-                ),
-                label: const Text(
-                  "Continue With Google",
-                  style: TextStyle(color: Colors.white),
-                )),
-            Row(
-              mainAxisAlignment: MainAxisAlignment.center,
-              children: [
-                const Text("You Dont Have An Account ?"),
-                TextButton(
-                    onPressed: () {
-                      Navigator.pushNamed(context, "/sign-up");
-                    },
-                    child: const Text("click here"))
-              ],
-            )
-          ],
-        ),
-      ),
-    );
+            );
+          })),
+        ));
   }
 
   Future<void> SignIn() async {
--- lib/screens/register/register.dart
+++ lib/screens/register/register.dart
@@ -1,7 +1,9 @@
+import 'package:client/bloc/register/register_bloc.dart';
 import 'package:client/widgets/Button.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/src/foundation/key.dart';
 import 'package:flutter/src/widgets/framework.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
 
 class Register extends StatefulWidget {
   const Register({Key? key}) : super(key: key);
@@ -25,52 +27,73 @@ class _RegisterState extends State<Register> {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(title: const Text("Sign In")),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: [
-            SizedBox(
-              width: 350,
-              child: TextField(
-                controller: _username,
-                decoration: const InputDecoration(
-                    label: Text("Username"), prefixIcon: Icon(Icons.person)),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            SizedBox(
-              width: 350,
-              child: TextField(
-                controller: _email,
-                keyboardType: TextInputType.emailAddress,
-                decoration: const InputDecoration(
-                    label: Text("Email"), prefixIcon: Icon(Icons.email)),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(5)),
-            SizedBox(
-              width: 350,
-              child: TextField(
-                controller: _password,
-                obscureText: true,
-                decoration: const InputDecoration(
-                    label: Text("Password"), prefixIcon: Icon(Icons.lock)),
-              ),
-            ),
-            const Padding(padding: EdgeInsets.all(20)),
-            Button(
-                size: const Size(350, 50), text: "Sign In", onPressed: () {}),
-          ],
-        ),
-      ),
-    );
-  }
+    _onRegisterSubmit() {
+      BlocProvider.of<RegisterBloc>(context).add(RegisterSubmit(
+          username: _username.text,
+          email: _email.text,
+          password: _password.text));
+    }
 
-  Future<void> SignUp() async {
-    final username = _username.text;
-    final email = _email.text;
-    final password = _password.text;
+    return Scaffold(
+        appBar: AppBar(title: const Text("Sign In")),
+        body: BlocListener(
+          listener: ((context, state) {
+            if (state is RegisterFailure) {
+              ScaffoldMessenger.of(context).showSnackBar(const SnackBar(
+                content: Text("Sign Up Failes"),
+                backgroundColor: Colors.red,
+              ));
+            }
+          }),
+          child: BlocBuilder(
+            builder: (context, state) {
+              return Center(
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: [
+                    SizedBox(
+                      width: 350,
+                      child: TextField(
+                        controller: _username,
+                        decoration: const InputDecoration(
+                            label: Text("Username"),
+                            prefixIcon: Icon(Icons.person)),
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(5)),
+                    SizedBox(
+                      width: 350,
+                      child: TextField(
+                        controller: _email,
+                        keyboardType: TextInputType.emailAddress,
+                        decoration: const InputDecoration(
+                            label: Text("Email"),
+                            prefixIcon: Icon(Icons.email)),
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(5)),
+                    SizedBox(
+                      width: 350,
+                      child: TextField(
+                        controller: _password,
+                        obscureText: true,
+                        decoration: const InputDecoration(
+                            label: Text("Password"),
+                            prefixIcon: Icon(Icons.lock)),
+                      ),
+                    ),
+                    const Padding(padding: EdgeInsets.all(20)),
+                    Button(
+                        size: const Size(350, 50),
+                        text: "Sign In",
+                        onPressed: () {
+                          _onRegisterSubmit();
+                        }),
+                  ],
+                ),
+              );
+            },
+          ),
+        ));
   }
 }
